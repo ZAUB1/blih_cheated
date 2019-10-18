0409d2fcff20687899fa9138e0834d03ef434b83
ZAUB1 <aub1@protonmail.ch>
2019-10-18 09:09:41 +0200

    Install script

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-echo "WIP"
-exit 1
-
 if [[ $EUID -ne 0 ]]; then
     echo "You must run this with superuser priviliges.  Try \"sudo ./install.sh\"" 2>&1
     exit 1
@@ -8,21 +5,10 @@ else
     echo "Installing blih cheated..."
 fi
 
-mkdir /bin/zblih_src
-cp -rf ./* /bin/zblih_src
+npm install
+sudo npm i -g @zeit/ncc
 
-cd /bin/zblih_src
-sudo npm install
-
-if [ -f ~/.zshrc ]; then
-    sudo echo alias zblih='node /bin/zblih_src/main.js' >> ~/.zshrc
-elif [ -f ~/.bashrc ]; then
-    sudo echo alias zblih='node /bin/zblih_src/main.js' >> ~/.bashrc
-else
-    echo "No shell rc found exiting"
-    exit 1
-fi
-
-sudo alias zblih="node /bin/zblih_src/main.js"
+ncc build main.js -m -o build
+sudo cp build/index.js /bin/zblih
 
 echo "Blih cheated installed (you might have to restart your terminal)"
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+#!/usr/bin/node
+
 const Blih = require("./blih");
 const CBS = [];
 
